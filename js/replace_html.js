145480a3e0380eb82e84927dc1754e9dc38f1f29
Eric Roy <royalmo6@gmail.com>
2022-03-22 00:18:13 +0100

    Moved files.

---
+++ js/replace_html.js
@@ -0,0 +1,46 @@
+/*
+This file is executed when needing to replace some parts
+of the https://ocwitic.epsem.upc.edu webpage.
+
+It adds some extra links to prevent forced downloads.
+It also adds some extra links to download all things at once.
+
+This file is part of OCW-Anti-Download extension, made
+by Eric Roy (royalmo). Find it and its liscence at
+https://github.com/royalmo/ocw-anti-download .
+*/
+
+function replace_html() {
+    // Removing fored download
+    var links = document.querySelectorAll("a");
+    var i;
+
+    for (i = 0; i<links.length; i++) {
+        if (links[i].href.includes("@@download")) {
+            links[i].parentElement.innerHTML = links[i].parentElement.innerHTML + '<a href="' + links[i].href.replace("@@download", "@@display-file") + '" style="color:grey;font-size:10px">[<em>View</em>]</a>';
+        }
+    }
+
+    // Adding extra download links
+    var spans = document.querySelectorAll("span");
+
+    for (i = 0; i<spans.length; i++) {
+        if (spans[i].classList.contains('summary') && spans[i].children[0].classList.contains('contenttype-file')) {
+            spans[i].innerHTML = spans[i].innerHTML + '<a href = "' + spans[i].children[0].href.replace('/view', '') + '" style="color:grey;font-size:10px">[<em>View</em>]</a>';
+        }
+    }
+
+    // Adding download subjects links
+    var subjects = document.querySelectorAll("dd");
+
+    for (i = 0; i<subjects.length; i++) {
+        if (subjects[i].classList.contains('doormatSectionBody')) {
+            subjects[i].innerHTML = subjects[i].innerHTML + '<a class="ocw-anti-d-lnk" style="color:#2F4F4F; font-size:10px;" href="#" lnk="' + subjects[i].firstChild.href + '"> [<em>Download all</em>]</a>'
+        }
+    }
+
+    console.log("OCW-A-D: Done replacing links!");
+}
+
+// For the moment this functionality is always active.
+replace_html();
