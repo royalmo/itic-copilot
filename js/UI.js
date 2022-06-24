2685a0d5dd74ca224c00c0179d0be5692cc657ae
FlyingKoala01 <isaac.iglesias.vila19@gmail.com>
2022-06-24 23:07:52 +0200

    Added switching between Divs in landpage

--- js/UI.js
+++ js/UI.js
@@ -1,3 +1,26 @@
+$(document).ready(function(){
+    $("#aboutButton").click(function(){
+        console.log("SHOW ABOUT");   
+        $("#settingDiv").hide();
+        $("#aboutDiv").show();
+        $("#landpageDiv").hide();
+        });
+    $("#settingButton").click(function(){
+        console.log("SHOW SETTINGS");
+        $("#settingDiv").show();
+        $("#aboutDiv").hide();
+        $("#landpageDiv").hide();
+    });
+    $("#langpageButton").click(function(){
+        console.log("SHOW LANDPAGE");
+        $("#settingDiv").hide();
+        $("#aboutDiv").hide();
+        $("#landpageDiv").show();
+        });
+
+   });
+
+
 function hello_world(){
     console.log("Hello World!");
 }
