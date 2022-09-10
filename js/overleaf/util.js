16c7228d382bdeb01e79d5e174362f51f084fe8b
Eric Roy <royalmo6@gmail.com>
2022-09-10 20:10:05 +0200

    Done except for cors policy...

--- js/overleaf/util.js
+++ js/overleaf/util.js
@@ -31,6 +31,11 @@
     $('.online-users').after('<div class="toolbar-item"><button id="copilot_escriny_commit" class="btn btn-full-height"><i class="fa fa-upload fa-fw"></i><p class="toolbar-label">Commit on escriny</p></button></div>');
 
     $('#copilot_escriny_commit').click(async function () {
+    
+        itic_copilot.fnon.confirm("Commit on Escriny",
+            await itic_copilot.load_partial("html/commit_menu.html"),
+            "Commit", "Cancel", function(){}
+        );
 
         data = await new JSZip.external.Promise(function (resolve, reject) {
             JSZipUtils.getBinaryContent( window.location.href + '/download/zip', function(err, data) {
@@ -42,11 +47,36 @@
             });
         })
         zip = await JSZip.loadAsync(data);
-
         texts = zip.file(/^.+\.tex$/);
-        tex = await texts[0].async("string");
 
-        console.log(tex);
+        if (texts.length == 0) {
+            $('.f__btn').first().click();
+            itic_copilot.fnon.alert("No TEX file found on the project.", "No TEX file found");
+            return;
+        }
 
+        file_contents = await texts[0].async("string");
+        var repository_id, file_path, commit_message;
+        
+        $('.copilot_input').change(function () {
+            repository_id = $('#copilot_escriny_repo').val();
+            file_path = $('#copilot_escriny_path').val();
+            commit_message = $('#copilot_commit_msg').val();
+        });
+
+        $('.f__btn[data-evt="tr"]').click(async function () {
+            console.log(repository_id);
+            repository_url = 'https://escriny.epsem.upc.edu/svn/' + repository_id;
+            auth = await itic_copilot.get_basic_auth();
+            // SVNJS 0.1.0
+            var svn = new SVN(auth, repository_url);
+            svn.add(file_path, file_contents);
+            svn.commit(commit_message, function () {
+                itic_copilot.fnon.alert(
+                    t('escriny_commit_completed_text'),
+                    t('escriny_commit_completed_title')
+                );
+            });
+        });
     });
 })();
